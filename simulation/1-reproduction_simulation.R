149b29e617dc5afa67cd031af86811f086522d43
Menglu Che <cathy900802@gmail.com>
2025-02-16 15:18:21 -0500

    update simulation codes

--- simulation/1-reproduction_simulation.R
+++ simulation/1-reproduction_simulation.R
@@ -1,24 +1,268 @@
 # Simulation of Ball Impurity 
 # This file provides the data generation process and the random seed to reproduce
-# Tables 1-3 corresponding to Models 2.1a, 2.1b, 2.2, 2.3, 2.4, 3.1, 3.2, and 3.3
-# To reproduce the corresponding simulation results, please specify the model number
-# in the form of a character array, e.g. "2.1" as the input of the wrapper. 
-# For Models 3.1-3.3, an outlier ratio (percentage) is also needed as "ops",
-# the default is set to 1.
+# Figures 1, 2, 4, and Tables 1-3 corresponding to Models 2.1a, 2.1b, 2.2, 2.3, 
+# 2.4, 3.1, 3.2, and 3.3.
+# Each set of simulations/numerical demonstrations is labeled, and an RData file,
+# or pdf file(s) of the figure(s), with be saved. The file will be named using 
+# the Figure or Table number as well as the model number.
+
+
+#################################
+############ Figure 1 ###########
+#################################
+
+# Output to be save as 
+
+rm(list=ls())
+library(shapes)
+library(Rcpp)
+library(Ball)    # for Ball Correlation
+library(energy)  # for Distance Correlation
+
+sourceCpp("BallDistanceVector.cpp")
+sourceCpp("BallImpurity.cpp")
+repeat.time=100
+#Gaussion
+n=500
+p=2
+d=0.5
+e_seq=c(20:50)/50
+BI_seq_mean=rep(0,length(e_seq))
+Dcov_seq_mean=rep(0,length(e_seq))
+Bcov_seq_mean=rep(0,length(e_seq))
+Sum_var_seq_mean=rep(0,length(e_seq))
+
+for(r in 1:repeat.time)
+{
+  BI_seq=rep(NA,length(e_seq))
+  Dcov_seq=rep(NA,length(e_seq))
+  Bcov_seq=rep(NA,length(e_seq))
+  Sum_var_seq=rep(NA,length(e_seq))
+  for(i in 1:length(e_seq))
+  {
+    e=e_seq[i] #noise level sou
+    Y=matrix(rnorm(n*p,mean=0, sd=e),nrow=n)
+    #plot(Y)
+    distance_matrix=BallDistanceVector(Y)
+    BI_seq[i]=BallImpurity(n,distance_matrix,1:n,d)
+    Dcov_seq[i]=dcov(Y,Y)
+    Bcov_seq[i]=bcov(Y,Y)
+    Sum_var_seq[i]=var(Y[,1])+var(Y[,2])
+  }
+  BI_seq_mean=BI_seq_mean+BI_seq
+  Dcov_seq_mean=Dcov_seq_mean+Dcov_seq
+  Bcov_seq_mean=Bcov_seq_mean+Bcov_seq
+  Sum_var_seq_mean=Sum_var_seq_mean+Sum_var_seq
+}
+
+result=cbind(BI_seq_mean,Bcov_seq_mean, Dcov_seq_mean,Sum_var_seq_mean)
+result=as.data.frame(scale(result))
+min.y=min(result)
+max.y=max(result)
+pdf("Figure1a_Model1.1.pdf",width=7,height=7)
+plot(e_seq,result$BI_seq_mean,pch=1,type="b",ylim=c(min.y,max.y),xlab="noise",ylab="",yaxt="n",cex.lab=2.5,cex.axis=2.5)
+lines(e_seq,result$Bcov_seq_mean,pch=2,type="b",col="blue")
+lines(e_seq,result$Dcov_seq_mean,pch=3,type="b",col="red")
+lines(e_seq,result$Sum_var_seq_mean,pch=4,type="b",col="green")
+legend("topleft", legend=c("BI","BCov", "DCov","SumVar"),
+       col=c("black","blue", "red","green"), lty=1,pch=1:4, cex=2,
+       box.lty=0)
+dev.off()
+
+##Mixtured model 1 Gaussion
+n=500
+p=2
+Y=matrix(NA,nrow=n,ncol=p)
+e=0.1
+d=0.2
+Pi_seq=seq(0,1,0.05)
+n_seq=Pi_seq*n
+BI_seq_mean=rep(0,length(Pi_seq))
+Bcov_seq_mean=rep(0,length(Pi_seq))
+Dcov_seq_mean=rep(0,length(Pi_seq))
+Sum_var_seq_mean=rep(0,length(Pi_seq))
+
+for(r in 1:repeat.time)
+{
+  BI_seq=rep(NA,length(Pi_seq))
+  Bcov_seq=rep(NA,length(Pi_seq))
+  Dcov_seq=rep(NA,length(Pi_seq))
+  Sum_var_seq=rep(NA,length(Pi_seq))
+  #### sample 2n Gaussian distributed points, and truncate 
+  Y1=matrix(rep(0,n*4),nrow=2*n)
+  Y2=matrix(rep(0,n*4),nrow=2*n)
+  mu1=c(2,2)
+  mu2=c(2,-2)
+  for(j in 1:2){
+    Y1[,j]=rnorm(2*n,mean=mu1[j],sd=e)
+    Y2[,j]=rnorm(2*n,mean=mu2[j],sd=e)
+  }
+  d1=apply(Y1-rep(mu1,each=nrow(Y1)),1,norm,type = "2")
+  d2=apply(Y2-rep(mu2,each=nrow(Y2)),1,norm,type = "2")
+  id1=which(d1<=0.5)
+  id2=which(d2<=0.5)
+  for(i in 1:length(Pi_seq))
+  {
+    n1=n*Pi_seq[i]
+    n2=n-n1
+    Y.t=rbind(Y1[sample(id1,size=n1),],Y2[sample(id2,size=n2),])
+    Y=list()
+    for(k in 1:nrow(Y.t)){Y[[k]]<-Y.t[k,]}
+    distance_matrix=BallDistanceVector(Y)
+    BI_seq[i]=BallImpurity(n,distance_matrix,1:n,d)
+    Dcov_seq[i]=dcov(Y.t,Y.t)
+    Bcov_seq[i]=bcov(Y.t,Y.t)
+    Sum_var_seq[i]=var(Y.t[,1])+var(Y.t[,2])
+    #plot(Y)
+  }
+  print(BI_seq)
+  BI_seq_mean=BI_seq_mean+BI_seq
+  Bcov_seq_mean=Bcov_seq_mean+Bcov_seq
+  Dcov_seq_mean=Dcov_seq_mean+Dcov_seq
+  Sum_var_seq_mean=Sum_var_seq_mean+Sum_var_seq
+}
+result=cbind(BI_seq_mean,Bcov_seq_mean,Dcov_seq_mean,Sum_var_seq_mean)
+result=as.data.frame(scale(result))
+min.y=min(result)
+max.y=max(result)
+pdf("Figure1b_Model1.2.pdf",width=7,height=7)
+plot(Pi_seq,result$BI_seq_mean,pch=1,type="b",ylim=c(min.y,max.y),xlab=expression(pi),ylab="",yaxt="n",cex.lab=2.5, cex.axis=2.5)
+lines(Pi_seq,result$Bcov_seq_mean,pch=2,type="b",col="blue")
+lines(Pi_seq,result$Dcov_seq_mean,pch=3,type="b",col="red")
+lines(Pi_seq,result$Sum_var_seq_mean,pch=4,type="b",col="green")
+legend("bottom", legend=c("BI","Bcov", "Dcov","SumVar"),
+       col=c("black","blue","red","green"), lty=1,pch=1:4, cex=1.8,
+       box.lty=0)
+dev.off()
+
+##Mixtured model 3 
+n=500
+p=2
+Y=matrix(NA,nrow=n,ncol=p)
+e=0.1
+d=0.2
+Pi_seq=seq(0,pi/2,length.out = 20)
+
+BI_seq_mean=rep(0,length(Pi_seq))
+
+Bcov_seq_mean=rep(0,length(Pi_seq))
+Dcov_seq_mean=rep(0,length(Pi_seq))
+Sum_var_seq_mean=rep(0,length(Pi_seq))
+
+for(r in 1:repeat.time)
+{
+  BI_seq=rep(NA,length(Pi_seq))
+  Bcov_seq=rep(NA,length(Pi_seq))
+  Dcov_seq=rep(NA,length(Pi_seq))
+  Sum_var_seq=rep(NA,length(Pi_seq))
+  for(i in 1:length(Pi_seq))
+  {
+    Pi=runif(n,min=0,max=Pi_seq[i])+rbinom(n,1,0.5)*pi
+    Y[,1]=cos(Pi)+rnorm(n,mean=0,sd=e)
+    Y[,2]=sin(Pi)+rnorm(n,mean=0,sd=e)
+    plot(Y,xlim=c(-1,1),ylim=c(-1,1))
+    distance_matrix=BallDistanceVector(Y)
+    BI_seq[i]=BallImpurity(n,distance_matrix,1:n,d)
+    Dcov_seq[i]=dcov(Y,Y)
+    Bcov_seq[i]=bcov(Y,Y)
+    Sum_var_seq[i]=var(Y[,1])+var(Y[,2])
+  }
+  BI_seq_mean=BI_seq_mean+BI_seq
+  Bcov_seq_mean=Bcov_seq_mean+Bcov_seq
+  Dcov_seq_mean=Dcov_seq_mean+Dcov_seq
+  Sum_var_seq_mean=Sum_var_seq_mean+Sum_var_seq
+}
+
+
+result=cbind(BI_seq_mean,Bcov_seq_mean,Dcov_seq_mean,Sum_var_seq_mean)
+result=as.data.frame(scale(result))
+min.y=min(result)
+max.y=max(result)
+pdf("Figure1c_Model1.3.pdf",width=7,height=7)
+plot(Pi_seq,result$BI_seq_mean,pch=1,type="b",ylim=c(min.y,max.y+0.5),xlab=expression(psi),ylab="",yaxt="n",cex.lab=2.5,cex.axis=2.5)
+lines(Pi_seq,result$Bcov_seq_mean,pch=2,type="b",col="blue")  
+lines(Pi_seq,result$Dcov_seq_mean,pch=3,type="b",col="red")
+lines(Pi_seq,result$Sum_var_seq_mean,pch=4,type="b",col="green")
+legend("topleft", legend=c("BI","BCov", "DCov","SumVar"),
+       col=c("black","blue", "red","green"), lty=1,pch=1:4, cex=1.8,
+       box.lty=0)
+dev.off()
+##############
+
+
+##########################
+####### Figure 2 #########
+##########################
+rm(list=ls())
+library(Rcpp)
+sourceCpp("BallDistanceVector.cpp")
+sourceCpp("BallImpurity.cpp")
+pvec=c(0.1, 0.74, 1.47)
+
+n=500
+rep=100
+sd=0.1
+d=0.4
+set.seed(202303)
+results=NULL
+for(p in pvec){
+  pheno_list = list()
+  Z=runif(n,0,1)*p+pi*rbinom(n,size=1,prob=0.5)
+  Yx=rep(0,n)
+  Yy=rep(0,n)
+  for(i in 1:n){
+    eps=rnorm(2,0,sd)
+    x=cos(Z[i])+eps[1]
+    y=sin(Z[i])+eps[2]
+    Yx[i]=x
+    Yy[i]=y
+    pheno_list[[i]] = matrix(cbind(x,y),ncol = 2)
+  }
+  pdf(paste0("Figure2_cut_points",p,".pdf"),width =5,height=5)
+  plot(Yx,Yy,pch=20,xlab="",ylab="",xlim=c(-1.3,1.3),ylim=c(-1.3,1.3),cex.lab=2.2, cex.axis=2.2)
+  dev.off()
+  
+  alphavec=seq(0,pi,by=0.01)
+  PG_vec=rep(0,length(alphavec))
+  D=BallDistanceVector(pheno_list)#dist(pheno_list)
+  for(k in 1:length(alphavec)){
+    alpha=alphavec[k]
+    set1=which(Yy-tan(alpha)*Yx>=0)
+    set2=setdiff(1:n,set1)
+    m1=length(set1)
+    m2=length(set2)
+    if(m1==0 | m2==0){
+      PG_vec[k]=0
+    }else{
+      BI_full=BallImpurity(n,D,1:n,d)
+      BI_left=BallImpurity(n,D,set1,d)
+      BI_right=BallImpurity(n,D,set2,d)
+      PG_vec[k]=BI_full-m1/n*BI_left-m2/n*BI_right
+    }}
+  results=rbind(results,PG_vec)
+}
+for(j in 1:3){
+  pdf(paste0("Figure2_demo_md3_p=",pvec[j],".pdf"),width=5,height=5)
+  plot(alphavec,results[j,],xlab=expression(alpha),ylab="",yaxt="n",pch=20,cex.lab=2.2,cex.axis=2)
+  lines(alphavec,results[j,], col="red")
+  dev.off()
+}
+
+#########################
+####### Table 1 #########
+#########################
 
 rm(list=ls())
 parent.path<- ### specify your parent path
 source(paste0(parent.path,"simulation/simu_rank_matrix.R"))
-reproduction<-function(Model="2.1a",ops=1){
-  if(Model=="2.1a"){
-    library(Rcpp)
-    library(energy) #for Distance Correlation
-    library(Ball)
-    library(VGAM)
-    sourceCpp(paste0(parent.path,"code/BallDistanceVector.cpp"))
-    sourceCpp(paste0(parent.path,"code/BallImpurity.cpp"))
+library(Rcpp)
+library(energy) #for Distance Correlation
+library(Ball)
+library(VGAM)
+sourceCpp(paste0(parent.path,"code/BallDistanceVector.cpp"))
+sourceCpp(paste0(parent.path,"code/BallImpurity.cpp"))
     # calculate the frequency of the true signals being selected 
-    Sel.rate<-function(n,d=10,true.v,rank.mtx) {
+Sel.rate<-function(n,d=10,true.v,rank.mtx) {
       # Input
       # n        :  the sample size
       # d        :  coeficient of cutoffs
@@ -28,19 +272,19 @@ reproduction<-function(Model="2.1a",ops=1){
       # Output
       # rate     :  the proportions that every single active predictor is selected 
       #             for a given model size, which is defauted c[n/log(n)], in the 1,000 replications.
-      rank.mtx.sel<-rank.mtx[1:d,]
-      r<-min(dim(rank.mtx)[2],length(rank.mtx)) #repeat times
-      p0<-nrow(true.v)
-      R<-matrix(0,p0,r)
-      rate<-rep(NA,p0)
-      for (i in 1:p0) {
-        for (j in 1:r) {R[i,j]<-(min(abs(rank.mtx.sel[,j]-true.v[i,j]))==0) }
-        rate[i]<-mean(R[i,])
-      }
-      return(rate)
+  rank.mtx.sel<-rank.mtx[1:d,]
+  r<-min(dim(rank.mtx)[2],length(rank.mtx)) #repeat times
+  p0<-nrow(true.v)
+  R<-matrix(0,p0,r)
+  rate<-rep(NA,p0)
+  for (i in 1:p0) {
+    for (j in 1:r) {R[i,j]<-(min(abs(rank.mtx.sel[,j]-true.v[i,j]))==0) }
+      rate[i]<-mean(R[i,])
     }
+    return(rate)
+  }
     # calculate the frequency all true signals being selected.
-    Sel.rate.all<-function(n,d=10,true.v,rank.mtx) {
+  Sel.rate.all<-function(n,d=10,true.v,rank.mtx) {
       # Input
       # n        :  the sample size
       # d        :  coeficient of cutoffs
@@ -50,9 +294,9 @@ reproduction<-function(Model="2.1a",ops=1){
       # Output
       # rate     :  the proportions that every single active predictor is selected 
       #             for a given model size, which is defauted c[n/log(n)], in the 1,000 replications.
-      rank.mtx.sel<-rank.mtx[1:d,]
-      r<-min(dim(rank.mtx)[2],length(rank.mtx))
-      p0<-nrow(true.v)
+    rank.mtx.sel<-rank.mtx[1:d,]
+    r<-min(dim(rank.mtx)[2],length(rank.mtx))
+    p0<-nrow(true.v)
       R<-matrix(0,p0,r)
       rate<-rep(NA,p0)
       for (i in 1:p0) {
